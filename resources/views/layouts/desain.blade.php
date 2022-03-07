5e119b6f979cd9468b16c657e604a350b98a690a
Facronactz <facronactz@gmail.com>
2022-03-07 14:08:22 +0700

    update view

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 
     <!-- CSS -->
-    <link rel="stylesheet" href="{{ asset('css/desain.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/desain.css') }}"> -->
 
     <title>Prodamas Plus</title>
 </head>
